080d976ce22f0d9f6ff0fa13fecc0ed0db312d4b
Jason Truxal <17013863+jasonTRUXAL@users.noreply.github.com>
2025-02-24 18:30:52 -0800

    v1.1.1 enable gid

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,12 +6,16 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID; // Discord application ID (should probably rename, not client)
 const GUILD_ID = process.env.GUILD_ID;   // used only for development, storing for future use but not init release
 
-// declare the command(s), just one for now likely more later so prepare for that
+// declare the command(s)
 const commands = [
   new SlashCommandBuilder()
     .setName('clips')
     .setDescription('Screech a clip from mAc\'s Twitch')
     .toJSON(),
+];
+
+// guild commands, used for testing
+const guildCommands = [
   new SlashCommandBuilder()
     .setName('streamdammit')
     .setDescription('How many times have we asked mAc to stream?')
@@ -26,18 +30,19 @@ const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
   try {
 	// maybe be more cereal with console logs for debugging
     console.log('Started refreshing application (/) commands.');
+	
+    await rest.put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: globalCommands }
+    );
+    console.log('Successfully reloaded global commands.');
+	
     if (GUILD_ID) {
       await rest.put(
         Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-        { body: commands }
+        { body: globalCommands }
       );
       console.log('Successfully reloaded restricted server commands.');
-    } else {
-      await rest.put(
-        Routes.applicationCommands(CLIENT_ID),
-        { body: commands }
-      );
-      console.log('Successfully reloaded global commands.');
     }
   } catch (error) {
     console.error(error);
