fa95ac1551fe61c85b0af70c83c4a2afb224fc7a
Jason Truxal <17013863+jasonTRUXAL@users.noreply.github.com>
2025-03-18 22:15:21 -0700

    adding reaction support

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+	GatewayIntentBits.GuildMessageReactions
   ] 
 });
 
